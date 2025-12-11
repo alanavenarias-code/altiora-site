bffd6a4b284a74ac2ff243813e4260b24bb5b221
alanavendano <alanavenarias@gmail.com>
2025-12-11 15:02:53 -0600

    Update layout.tsx
    Typo

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Altiora COnsultoría",
+  title: "Altiora Consultoría",
   description: "Inteligencia territorial y electoral para Candidatos y Gobiernos",
 };
 
